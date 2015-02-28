7683dd26c3e1b628ca6b0075b9c55689bd00b603
mjeulinl <michael.jeulinl@gmail.com>
2015-02-28 14:07:36 +0100

    Add Maximal Distance function: Identifies the two indexes of the array with the maximal distance. Use functor as template argument to choose the distance computation Add a basic UT directed by the Simple Stock Market Problem. Remove all tabs from Search.hpp

--- Modules/Array/Search.hpp
+++ Modules/Array/Search.hpp
@@ -3,41 +3,83 @@
 
 // STD includes
 #include <cmath>
+#include <functional>
 #include <iostream>
 #include <limits>
 #include <vector>
 
 namespace ArrayAlgorithms
 {
-	/// Binary Search - Iterative
-  /// Search within a sorted vector the occurence of the key passed as parameter
+  /// Binary Search - Iterative
+  /// Proceed a dichotomic search within a sorted vector the first occurence
+  /// of the key passed as parameter
   ///
   /// @param sortedArray a sorted vector from within the search occurs
   /// @param key the key value to be searched
   ///
   /// @complexity O(log(N))
   /// @return The vector index of the first found key, -1 otherwise
-  template <class T>
-	int BinarySearchIterativeT(const std::vector<T>& sortedArray, const T& key)
+  template <typename T>
+  int BinarySearchIterativeT(const std::vector<T>& sortedArray, const T& key)
   {
     int index = -1;
-	  int mid; 
-	  int lowInd = 0; 
-	  int highInd = sortedArray.size() - 1; 
+    int mid;
+    int lowInd = 0;
+    int highInd = static_cast<int>(sortedArray.size() - 1);
 
-	  while(lowInd <= highInd && index < 0)
-	  {
-		  mid = (lowInd + highInd) / 2;
-		
+    while(lowInd <= highInd && index < 0)
+    {
+      mid = (lowInd + highInd) / 2;
+
       if (Equal(key, sortedArray[mid]))
-			  index = mid;
-		  else if (key > sortedArray[mid]) 
-			  lowInd = mid + 1;
-		  else
-			  highInd = mid - 1;
-	  }
-	
-	  return index;
+        index = mid;
+      else if (key > sortedArray[mid])
+        lowInd = mid + 1;
+      else
+        highInd = mid - 1;
+    }
+
+    return index;
+  }
+
+  /// MaxDistance
+  /// Identifies the two indexes of the array with the maximal distance.
+  ///
+  /// @detail Known as the simple stock market problem with the default functor (std::minus):
+  /// It finds i and j that maximizes Aj – Ai, where i < j.
+  /// In other words, maximizes the benefice of a resell given an array of prices varying over time.
+  ///
+  /// @param elements vector from within the search occurs
+  /// @param template functor type computing the distance between two elements
+  ///
+  /// @complexity O(N)
+  /// @return indexes of the array with the maximal distance, <-1,-1> in case of error
+  template <typename T, typename Distance /*= std::minus*/>
+  std::pair<int, int> MaxDistance(const std::vector<T>& elements)
+  {
+    if (elements.size() < 2)
+      return std::pair<int, int>(-1, -1);
+
+    const Distance distanceFunc = Distance();
+    std::pair<int, int> indexes(0, 0);
+    T maxDist = std::numeric_limits<T>::lowest();
+    for (std::vector<T>::const_iterator it = elements.begin() + 1; it != elements.end(); ++it)
+    {
+      const int currentIdx = std::distance(elements.begin(), it);
+
+      // Keeps track index of the minimum value
+      if (*it < elements[indexes.first])
+        indexes.first = currentIdx;
+
+      // Keeps track of the largest distance and its max value index
+      const T distance = distanceFunc(*it, elements[indexes.first]);
+      if (distance > maxDist) {
+        maxDist = distance;
+        indexes.second = currentIdx;
+      }
+    }
+
+    return indexes;
   }
 
   template <class T>
@@ -50,7 +92,7 @@ namespace ArrayAlgorithms
   bool Equal<>(const int& a, const int& b) 
   {
     return a == b;
-  }	
+  }
 };
 
 #endif() // MODULE_ARRAY_SEARCH_HXX
--- Modules/Array/Testing/TestBasicSearch.cpp
+++ Modules/Array/Testing/TestBasicSearch.cpp
@@ -3,7 +3,9 @@
 
 namespace {
   const int SortedIntArray[] = {-3, -2, 0, 2, 8, 15,36, 212, 366}; // Simple sorted array of integers with negative values
-  const float SortedFloatArray[] = {-.3, 0.0, 0.12, 2.5, 8};       // Simple sorted array of floats with negative values
+  const int RandomArrayInt[] = {4, 3, 5, 2, -1, 3, 2, 3, 4};       // Simple random array of integers with negative values
+  const double SortedFloatArray[] = {-.3, 0.0, 0.12, 2.5, 8};      // Simple sorted array of floats with negative values
+
 }
 
 // Basic BinarySearchIterative tests on a sorted array of ints
@@ -19,11 +21,11 @@ TEST(TestBasicSearch, BinarySearchIterativeTBasicInt)
 // Basic BinarySearchIterative tests on a sorted array of floats
 TEST(TestBasicSearch, BinarySearchIterativeTBasicFloat)
 {
-  std::vector<float> sortedFloatArray(SortedFloatArray, SortedFloatArray + sizeof(SortedFloatArray) / sizeof(float));
+  std::vector<double> sortedFloatArray(SortedFloatArray, SortedFloatArray + sizeof(SortedFloatArray) / sizeof(double));
 
-  EXPECT_EQ(0, ArrayAlgorithms::BinarySearchIterativeT<float>(sortedFloatArray, static_cast<const float>(-.3)));    // Find first value
-  EXPECT_EQ(2, ArrayAlgorithms::BinarySearchIterativeT<float>(sortedFloatArray, static_cast<const float>(0.12)));   // Find a random value owned by the vector
-  EXPECT_EQ(-1, ArrayAlgorithms::BinarySearchIterativeT<float>(sortedFloatArray, static_cast<const float>(8.1)));   // Try to find a non-existing value
+  EXPECT_EQ(0, ArrayAlgorithms::BinarySearchIterativeT<double>(sortedFloatArray, static_cast<const double>(-.3)));    // Find first value
+  EXPECT_EQ(2, ArrayAlgorithms::BinarySearchIterativeT<double>(sortedFloatArray, static_cast<const double>(0.12)));   // Find a random value owned by the vector
+  EXPECT_EQ(-1, ArrayAlgorithms::BinarySearchIterativeT<double>(sortedFloatArray, static_cast<const double>(8.1)));   // Try to find a non-existing value
 }
 
 // Test BinarySearchIterative does not loop with a single array value
@@ -38,5 +40,17 @@ TEST(TestBasicSearch, BinarySearchIterativeTNoLoop)
 // Test BinarySearchIterative does not fails on empty array
 TEST(TestBasicSearch, BinarySearchIterativeTEmptyArray)
 {
-  EXPECT_EQ(-1, ArrayAlgorithms::BinarySearchIterativeT<int>(std::vector<int>(), 5));    // Should return -1 on empty array
+  EXPECT_EQ(-1, ArrayAlgorithms::BinarySearchIterativeT<int>(std::vector<int>(), 5));   // Should return -1 on empty array
+}
+
+
+// Test SimpleStockMarketProblem on a simple market array
+TEST(TestBasicSearch, SimpleStockMarketProblem)
+{
+  std::vector<int> marketPrices (RandomArrayInt, RandomArrayInt + sizeof(RandomArrayInt) / sizeof(int));
+
+  // Should return <4,8> (largest benefice of 5)
+  std::pair<int, int> maxBeneficeIndexes = ArrayAlgorithms::MaxDistance<int, std::minus<int> >(marketPrices);
+  EXPECT_EQ(4, maxBeneficeIndexes.first);
+  EXPECT_EQ(8, maxBeneficeIndexes.second);
 }
