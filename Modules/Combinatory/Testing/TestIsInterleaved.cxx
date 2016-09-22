21fd344b2c24c85e66d1aaf5537f15d33ff53752
Michael Jeulin-L <michael.jeulinl@gmail.com>
2016-09-22 15:36:47 +0200

    ENH: Add IsInterleaved function
    - Add Documentation
    - Add UTs
    - First exaxmple using lambda functions
    - Start using auto

---
+++ Modules/Combinatory/Testing/TestIsInterleaved.cxx
@@ -0,0 +1,75 @@
+#include <gtest/gtest.h>
+#include <is_interleaved.hxx>
+
+using namespace SHA_Combinatory;
+
+#ifndef DOXYGEN_SKIP
+namespace {
+  const int kSequenceAInt[] = {4, 3, 5};              // Simple random array of integers with negative values
+  const int kSequenceBInt[] = {-2, 3, 5, 4};          // Simple random array of integers with negative values
+  const int kSequenceCInt[] = {5, -2, 4, 3, 4, 5, 3}; // Interleave of Sequence A and Sequence B
+  const int kSequenceDInt[] = {-2, 3};                // Simple random array of integers with negative values
+  const std::string kSequenceAStr = "acegm";          // Random string
+  const std::string kSequenceBStr = "xacvgeze";       // Random string
+  const std::string kSequenceCStr = "axaemgccvgeze";  // Interleave of Sequence A and Sequence B
+
+  typedef std::vector<int> Container_Type;
+  typedef Container_Type::iterator Iterator_Type;
+}
+#endif /* DOXYGEN_SKIP */
+
+// Test interleaves
+TEST(TestIsInterleaved, Interleaved)
+{
+  // Null interleave on empty sequences - empty interleave expected
+  {
+    Container_Type empty = Container_Type();
+    EXPECT_TRUE(IsInterleaved<Iterator_Type>
+      (empty.begin(), empty.end(), empty.begin(), empty.end(), empty.begin(), empty.end()));
+  }
+
+  // interleave with one empty sequence (first) - same sequence expected as interleave
+  {
+    Container_Type sequenceA(kSequenceAInt, kSequenceAInt + sizeof(kSequenceAInt) /
+      sizeof(Container_Type::value_type));
+    EXPECT_TRUE(IsInterleaved<Iterator_Type>(sequenceA.begin(), sequenceA.end(), sequenceA.end(),
+      sequenceA.end(), sequenceA.begin(), sequenceA.end()));
+  }
+
+  // interleave with one empty sequence (second) - same sequence expected as interleave
+  {
+    Container_Type sequenceA(kSequenceAInt, kSequenceAInt + sizeof(kSequenceAInt) /
+      sizeof(Container_Type::value_type));
+    EXPECT_TRUE(IsInterleaved<Iterator_Type>(sequenceA.end(), sequenceA.end(), sequenceA.begin(),
+      sequenceA.end(), sequenceA.begin(), sequenceA.end()));
+  }
+
+  // interleave with same sequence - same sequence cannot be interleave of both
+  {
+    Container_Type sequenceA(kSequenceAInt, kSequenceAInt + sizeof(kSequenceAInt) /
+      sizeof(Container_Type::value_type));
+    EXPECT_FALSE(IsInterleaved<Iterator_Type>(sequenceA.begin(), sequenceA.end(), sequenceA.begin(),
+      sequenceA.end(), sequenceA.begin(), sequenceA.end()));
+  }
+
+  // Normal run with interleave
+  {
+    Container_Type sequenceA(kSequenceAInt, kSequenceAInt + sizeof(kSequenceAInt) /
+      sizeof(Container_Type::value_type));
+    Container_Type sequenceB(kSequenceBInt, kSequenceBInt + sizeof(kSequenceBInt) /
+      sizeof(Container_Type::value_type));
+    Container_Type sequenceC(kSequenceCInt, kSequenceCInt + sizeof(kSequenceCInt) /
+      sizeof(Container_Type::value_type));
+    EXPECT_TRUE(IsInterleaved<Iterator_Type>(sequenceA.begin(), sequenceA.end(), sequenceB.begin(),
+      sequenceB.end(), sequenceC.begin(), sequenceC.end()));
+  }
+
+  // Normal run with interleave on strings
+  {
+    std::string aStr = kSequenceAStr;
+    std::string bStr = kSequenceBStr;
+    std::string cStr = kSequenceCStr;
+    EXPECT_TRUE(IsInterleaved<std::string::iterator>(aStr.begin(), aStr.end(), bStr.begin(),
+      bStr.end(), cStr.begin(), cStr.end()));
+  }
+}
---
+++ Modules/Combinatory/is_interleaved.hxx
@@ -0,0 +1,58 @@
+#ifndef MODULE_COMBINATORY_IS_INTERLEAVED_HXX
+#define MODULE_COMBINATORY_IS_INTERLEAVED_HXX
+
+// STD includes
+#include <map>
+
+namespace SHA_Combinatory
+{
+  /// IsInterleaved - Return whether or not if a sequence is the interleave of the two others.
+  ///
+  /// @complexity O(N + M + K).
+  ///
+  /// @tparam Iterator type using to go through the collection.
+  ///
+  /// @param beginFirst,endFirst,beginSecond,endSecond,beginFull,endFull - iterators to the initial and
+  /// final positions of the sequences. The range used is [first,last), which contains all the elements
+  /// between first and last, including the element pointed by first but not the element pointed by last.
+  ///
+  /// @return true if the last sequence is the interleave of the two others, false otherwise.
+  template <typename Iterator>
+  bool IsInterleaved(const Iterator& beginFirst, const Iterator& endFirst,
+                     const Iterator& beginSecond, const Iterator& endSecond,
+                     const Iterator& beginFull, const Iterator& endFull)
+  {
+    // Lambda that count each element occurence within the map "count"
+    std::map<Iterator::value_type, int> count;
+    auto lCountElement = [&count](Iterator::value_type val)
+    {
+      auto countIt = count.find(val);
+      if (countIt != count.end())
+        ++countIt->second;
+      else
+        count.emplace(val, 1);
+    };
+
+    // Count both sequences
+    std::for_each(beginFirst, endFirst, lCountElement);
+    std::for_each(beginSecond, endSecond, lCountElement);
+
+    // Decrease the count of each element given the full sequence
+    for (auto it = beginFull; it != endFull; ++it)
+    {
+      auto countIt = count.find(*it);
+      if (countIt == count.end())
+        return false;
+      else if (--countIt->second < 0)
+        return false;
+    }
+
+    // Fail if hte count is not equal to 0
+    for (auto it = count.begin(); it != count.end(); ++it)
+      if (it->second != 0) return false;
+
+    return true;
+  }
+};
+
+#endif() // MODULE_COMBINATORY_IS_INTERLEAVED_HXX
