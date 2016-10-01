509238177e084a24e29cf684b6b9e863e2b1eace
Michael Jeulin-L <michael.jeulinl@gmail.com>
2016-10-01 12:18:38 +0200

    DOC: Update all source files with proper license information header

--- Modules/Search/max_distance.hxx
+++ Modules/Search/max_distance.hxx
@@ -1,3 +1,22 @@
+/*===========================================================================================================
+ *
+ * SHA - Simple Hybesis Algorithms
+ *
+ * Copyright (c) Michael Jeulin-Lagarrigue
+ *
+ *  Licensed under the MIT License, you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         https://github.com/michael-jeulinl/Simple-Hybesis-Algorithms/blob/master/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ *=========================================================================================================*/
 #ifndef MODULE_SEARCH_MAX_DISTANCE_HXX
 #define MODULE_SEARCH_MAX_DISTANCE_HXX
 
